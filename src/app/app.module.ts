79a6d88ec25111be1ac5fe5540368085dd4a6cae
mordini <yahewisthedevil@yahoo.com>
2018-08-27 21:54:25 -0400

    Section 2 Lecture 30 - Two-Way-Databinding

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './server/server.component';
@@ -16,7 +17,8 @@ import { SuccessAlertComponent } from './alerts/successalert/successalert.compon
     SuccessAlertComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
