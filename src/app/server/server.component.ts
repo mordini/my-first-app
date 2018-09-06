38644ce43003556bf7c54e9360b14de2bdccd62a
mordini <yahewisthedevil@yahoo.com>
2018-09-06 18:56:24 -0400

    moved templateUrl for some reason

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
   styles: [`
     .online {
       color: white;
